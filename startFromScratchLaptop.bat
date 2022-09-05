d76fe2888559e8b1ab35772a21748aa7622c531d
Dario Turchi <61662791+0xDario@users.noreply.github.com>
2022-09-04 21:25:02 -0400

    split files into Desktop/Laptop scripts

---
+++ startFromScratchLaptop.bat
@@ -0,0 +1,133 @@
+@echo off
+
+REM 1Password
+winget install -e AgileBits.1Password -h
+if %ERRORLEVEL% EQU 0 Echo 1Password installed successfully.
+
+REM 7Zip
+winget install -e 7zip.7zip -h
+if %ERRORLEVEL% EQU 0 Echo 7Zip installed successfully.
+
+REM Adobe Acrobat Reader DC
+winget install -e Adobe.Acrobat.Reader.64-bit
+if %ERRORLEVEL% EQU 0 Echo Adobe Acrobat Reader DC (64-bit) installed successfully.
+
+REM Android Studio
+winget install -e Google.AndroidStudio
+if %ERRORLEVEL% EQU 0 Echo Android Studio installed successfully.
+
+REM Brave
+winget install -e BraveSoftware.BraveBrowser -h
+if %ERRORLEVEL% EQU 0 Echo Brave installed successfully.
+
+REM Chrome
+winget install -e Google.Chrome -h
+if %ERRORLEVEL% EQU 0 Echo Chrome installed successfully.
+
+REM Discord
+winget install -e Discord.Discord -h
+if %ERRORLEVEL% EQU 0 Echo Discord installed successfully.
+
+REM Etcher
+winget install -e Balena.Etcher -h
+if %ERRORLEVEL% EQU 0 Echo Etcher installed successfully.
+
+REM Firefox
+winget install -e Mozilla.Firefox -h
+if %ERRORLEVEL% EQU 0 Echo Firefox installed successfully.
+
+REM flux
+winget install -e flux.flux -h
+if %ERRORLEVEL% EQU 0 Echo Flux installed successfully.
+
+REM Git
+winget install -e Git.Git -h
+if %ERRORLEVEL% EQU 0 Echo Git installed successfully.
+
+REM GitHub Desktop
+winget install -e GitHub.GitHubDesktop -h
+if %ERRORLEVEL% EQU 0 Echo GitHub Desktop installed successfully.
+
+REM Greenshot
+winget install -e Greenshot.Greenshot -h
+if %ERRORLEVEL% EQU 0 Echo Greenshot installed successfully.
+
+REM grepWin
+winget install -e StefansTools.grepWin -h
+if %ERRORLEVEL% EQU 0 Echo grepWin installed successfully.
+
+REM NordVPN
+winget install -e NordVPN.NordVPN -h
+if %ERRORLEVEL% EQU 0 Echo NordVPN installed successfully.
+
+REM Malwarebytes
+winget install -e Malwarebytes.Malwarebytes -h
+if %ERRORLEVEL% EQU 0 Echo Malwarebytes installed successfully.
+
+REM OBS Studio
+winget install -e OBSProject.OBSStudio -h
+if %ERRORLEVEL% EQU 0 Echo OBS Studio installed successfully.
+
+REM Obsidian
+winget install -e Obsidian.Obsidian -h
+if %ERRORLEVEL% EQU 0 Echo OBS Studio installed successfully.
+
+REM PowerToys
+winget install -e Microsoft.PowerToys -h
+if %ERRORLEVEL% EQU 0 Echo Powertoys installed successfully.
+
+REM qBittorrent
+winget install -e qBittorrent.qBittorrent -h
+if %ERRORLEVEL% EQU 0 Echo qBittorrent installed successfully.
+
+REM Signal
+winget install -e OpenWhisperSystems.Signal -h
+if %ERRORLEVEL% EQU 0 Echo Signal installed successfully.
+
+REM Spotify
+winget install -e Spotify.Spotify -h
+if %ERRORLEVEL% EQU 0 Echo Spotify installed successfully.
+
+REM Teams
+winget install -e Microsoft.Teams -h
+if %ERRORLEVEL% EQU 0 Echo Microsoft Teams installed successfully.
+
+REM TeamViewer
+winget install -e TeamViewer.TeamViewer -h
+if %ERRORLEVEL% EQU 0 Echo  TeamViewer installed successfully.
+
+REM Telegram
+winget install -e Telegram.TelegramDesktop -h
+if %ERRORLEVEL% EQU 0 Echo Telegram installed successfully.
+
+REM Terminal
+winget install -e Microsoft.WindowsTerminal -h
+if %ERRORLEVEL% EQU 0 Echo Terminal installed successfully.
+
+REM Trading View
+winget install -e TradingView.TradingViewDesktop -h
+if %ERRORLEVEL% EQU 0 Echo Trading View installed successfully.
+
+REM TreeSize Free
+winget install -e JAMSoftware.TreeSize.Free -h
+if %ERRORLEVEL% EQU 0 Echo TreeSizeFree installed successfully.
+
+REM Ubuntu
+winget install -e Canonical.Ubuntu -h
+if %ERRORLEVEL% EQU 0 Echo Ubuntu installed successfully.
+
+REM VirtualBox
+winget install -e Oracle.VirtualBox -h
+if %ERRORLEVEL% EQU 0 Echo VirtualBox installed successfully.
+
+REM Visual Studio Code
+winget install -e Microsoft.VisualStudioCode -h
+if %ERRORLEVEL% EQU 0 Echo Visual Studio Code installed successfully.
+
+REM Visual Studio Community 2022
+winget install -e Microsoft.VisualStudio.2022.Community -h
+if %ERRORLEVEL% EQU 0 Echo Microsoft.VisualStudio.2019.Community installed successfully.
+
+REM VLC
+winget install -e VideoLAN.VLC -h
+if %ERRORLEVEL% EQU 0 Echo VLC installed successfully.
