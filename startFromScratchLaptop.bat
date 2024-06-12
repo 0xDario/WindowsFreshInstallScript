ff400870f634bc4e8e8b4e79e5463848df9a28a2
Dario Turchi <darioturchi@outlook.com>
2024-06-12 17:21:15 -0400

    - Added --accept-package-agreements and --accept-source-agreements parameters to all winget install commands to automatically accept package agreements. - Implemented a check for administrative privileges at the beginning of the script. - Modified the script to relaunch with elevated privileges if not already running as an administrator. - Ensured all applications are installed with necessary permissions and agreement acceptance.

--- startFromScratchLaptop.bat
+++ startFromScratchLaptop.bat
@@ -1,11 +1,18 @@
 @echo off
+:: Check if the script is running with elevated privileges
+openfiles >nul 2>&1
+if not %errorlevel%==0 (
+    echo Requesting administrative privileges...
+    powershell -Command "Start-Process '%0' -ArgumentList '%*' -Verb RunAs"
+    exit /b
+)
 
 REM 1Password
-winget install -e AgileBits.1Password -h
+winget install -e AgileBits.1Password -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo 1Password installed successfully.
 
 REM 7Zip
-winget install -e 7zip.7zip -h
+winget install -e 7zip.7zip -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo 7Zip installed successfully.
 
 REM Adobe Acrobat Reader DC
@@ -13,149 +20,149 @@ winget install -e Adobe.Acrobat.Reader.64-bit
 if %ERRORLEVEL% EQU 0 Echo Adobe Acrobat Reader DC (64-bit) installed successfully.
 
 REM Brave
-winget install -e Brave.Brave -h
+winget install -e Brave.Brave -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo Brave installed successfully.
 
 REM Chrome
-winget install -e Google.Chrome -h
+winget install -e Google.Chrome -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo Chrome installed successfully.
 
 REM CPUID CPU-Z
-winget install -e CPUID.CPU-Z -h
+winget install -e CPUID.CPU-Z -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo CPU-Z installed successfully.
 
 REM CPUID HWMonitor
-winget install -e CPUID.HWMonitor -h
+winget install -e CPUID.HWMonitor -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo HWMonitor installed successfully.
 
 REM Discord
-winget install -e Discord.Discord -h
+winget install -e Discord.Discord -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo Discord installed successfully.
 
 REM Etcher
-winget install -e Balena.Etcher -h
+winget install -e Balena.Etcher -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo Etcher installed successfully.
 
 REM Firefox
-winget install -e Mozilla.Firefox -h
+winget install -e Mozilla.Firefox -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo Firefox installed successfully.
 
 REM flux
-winget install -e flux.flux -h
+winget install -e flux.flux -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo Flux installed successfully.
 
 REM GIMP
-winget install -e GIMP.GIMP -h
+winget install -e GIMP.GIMP -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo GIMP installed successfully.
 
 REM Git
-winget install -e Git.Git -h
+winget install -e Git.Git -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo Git installed successfully.
 
 REM Gpg4win
-winget install -e GnuPG.Gpg4win -h
+winget install -e GnuPG.Gpg4win -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo Gpg4win installed successfully.
 
 REM grepWin
-winget install -e StefansTools.grepWin -h
+winget install -e StefansTools.grepWin -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo grepWin installed successfully.
 
 REM Malwarebytes
-winget install -e Malwarebytes.Malwarebytes -h
+winget install -e Malwarebytes.Malwarebytes -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo Malwarebytes installed successfully.
 
 REM MullvadVPN
-winget install -e MullvadVPN.MullvadVPN -h
+winget install -e MullvadVPN.MullvadVPN -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo MullvadVPN installed successfully.
 
 REM MusicBee
-winget install -e MusicBee.MusicBee -h
+winget install -e MusicBee.MusicBee -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo MusicBee installed successfully.
 
 REM Microsoft Visual Studio Code
-winget install -e Microsoft.VisualStudioCode -h
+winget install -e Microsoft.VisualStudioCode -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo Visual Studio Code installed successfully.
 
 REM Microsoft Visual Studio Community 2022
-winget install -e Microsoft.VisualStudio.2022.Community -h
+winget install -e Microsoft.VisualStudio.2022.Community -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo Microsoft.VisualStudio.2022.Community installed successfully.
 
 REM Microsoft SQL Server 2019 Developer
-winget install -e Microsoft.SQLServer.2019.Developer -h
+winget install -e Microsoft.SQLServer.2019.Developer -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo Microsoft SQL Server 2019 Developer installed successfully.
 
 REM Microsoft SQL Server Management Studio
-winget install -e Microsoft.SQLServerManagementStudio -h
+winget install -e Microsoft.SQLServerManagementStudio -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo Microsoft SQL Server Management Studio installed successfully.
 
 REM OBS Studio
-winget install -e OBSProject.OBSStudio -h
+winget install -e OBSProject.OBSStudio -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo OBS Studio installed successfully.
 
 REM Obsidian
-winget install -e Obsidian.Obsidian -h
+winget install -e Obsidian.Obsidian -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo OBS Studio installed successfully.
 
 REM Postman
-winget install -e Postman.Postman -h
+winget install -e Postman.Postman -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo Postman installed successfully.
 
 REM PowerToys
-winget install -e Microsoft.PowerToys -h
+winget install -e Microsoft.PowerToys -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo Powertoys installed successfully.
 
 REM PuTTY
-winget install -e PuTTY.PuTTY -h
+winget install -e PuTTY.PuTTY -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo PuTTY installed successfully.
 
 REM qBittorrent
-winget install -e qBittorrent.qBittorrent -h
+winget install -e qBittorrent.qBittorrent -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo qBittorrent installed successfully.
 
 REM Signal
-winget install -e OpenWhisperSystems.Signal -h
+winget install -e OpenWhisperSystems.Signal -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo Signal installed successfully.
 
 REM Spotify
-winget install -e Spotify.Spotify -h
+winget install -e Spotify.Spotify -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo Spotify installed successfully.
 
 REM Sourcetree
-winget install -e Atlassian.Sourcetree -h
+winget install -e Atlassian.Sourcetree -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo Sourcetree installed successfully.
 
 REM TeamViewer
-winget install -e TeamViewer.TeamViewer -h
+winget install -e TeamViewer.TeamViewer -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo  TeamViewer installed successfully.
 
 REM Telegram
-winget install -e Telegram.TelegramDesktop -h
+winget install -e Telegram.TelegramDesktop -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo Telegram installed successfully.
 
 REM Terminal
-winget install -e Microsoft.WindowsTerminal -h
+winget install -e Microsoft.WindowsTerminal -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo Terminal installed successfully.
 
 REM TickTick
-winget install -e Appest.TickTick -h
+winget install -e Appest.TickTick -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo  TeamViewer installed successfully.
 
 REM Trading View
-winget install -e TradingView.TradingViewDesktop -h
+winget install -e TradingView.TradingViewDesktop -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo Trading View installed successfully.
 
 REM TreeSize Free
-winget install -e JAMSoftware.TreeSize.Free -h
+winget install -e JAMSoftware.TreeSize.Free -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo TreeSizeFree installed successfully.
 
 REM Ubuntu
-winget install -e Canonical.Ubuntu -h
+winget install -e Canonical.Ubuntu -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo Ubuntu installed successfully.
 
 REM VirtualBox
-winget install -e Oracle.VirtualBox -h
+winget install -e Oracle.VirtualBox -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo VirtualBox installed successfully.
 
 REM VLC
-winget install -e VideoLAN.VLC -h
+winget install -e VideoLAN.VLC -h --accept-package-agreements --accept-source-agreements
 if %ERRORLEVEL% EQU 0 Echo VLC installed successfully.
